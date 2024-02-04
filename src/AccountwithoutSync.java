6578b07b61f30592e0de8c7f72e052e9d19dbc58
forughshabani90 <forugh.shabani.90@gmail.com>
2024-02-03 23:51:13 -0800

    first commit

---
+++ src/AccountwithoutSync.java
@@ -0,0 +1,39 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class AccountwithoutSync {
+    private static Account account=new Account();
+
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newCachedThreadPool();
+        for (int i=0;i<100;i++){
+            executor.execute(new AddPenyTask());
+        }
+        executor.shutdown();
+        while (!executor.isTerminated()){
+
+        }
+        System.out.println("what is balance?" + account.getBalance());
+    }
+    private static class AddPenyTask implements  Runnable{
+        public void run(){
+            account.deposit(1);
+        }
+    }
+    private static class Account{
+        private int balance=0;
+        public int getBalance(){
+            return balance;
+        }
+        public void deposit(int amount){
+            int newbalance=balance+amount;
+            try {
+                Thread.sleep(5);
+            }
+            catch (InterruptedException ex){
+
+            }
+            balance=newbalance;
+        }
+    }
+}
